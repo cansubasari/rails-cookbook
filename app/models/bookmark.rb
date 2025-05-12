5090789e6d4b9d26433602515b349c5fddf73dc9
dzhansu bashara <cansubasari@gmail.com>
2025-05-12 10:48:47 +0100

    added validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
+
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates :recipe_id, uniqueness: { scope: :category_id, message: "has already been bookmarked in this category" }
 end
+git
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ApplicationRecord
+  has_many: bookmarks, dependent: :destroy
+  has_many: categories, through: :bookmarks
+
+  validates :name,  presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :image_url, presence: true
+  validates :rating, presence: true, numericality: { in: 0..5 }
 end
